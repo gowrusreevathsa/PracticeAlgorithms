828058d2acbc89b280dd5e630f96af21e87a8db1
gowrusreevathsa <sreevathsagowru@gmail.com>
2019-10-14 14:52:45 +0530

    Reverse a stack using recursion

---
+++ Stack/Reverse a stack using recursion.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//void sortUtil(int t, stack<int>& s){
+//	
+//	int temp;
+//	
+//	if(s.empty() || t>s.top()){
+//		s.push(t);
+//	}
+//	else{
+//		temp = s.top();
+//		s.pop();
+//		sortUtil(t, s);
+//		s.push(temp);
+//	}
+//	
+//}
+
+void rev_stack(stack<int>& s){
+	int temp;
+	
+	if(!s.empty()){
+		temp = s.top();
+		s.pop();
+		rev_stack(s);
+		s.push(temp);
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin >> n;	//no. of stack elements
+	
+	stack<int> s;
+	
+	int t;
+	for(int i = 0; i<n; ++i){
+		cin >> t;
+		s.push(t);
+	}
+	
+	rev_stack(s);
+	
+	//printing stack
+	cout << "\n\n";
+	while(!s.empty()){
+		cout << s.top() << "\n";
+		s.pop();
+	}
+	
+	return 0;
+}
+
