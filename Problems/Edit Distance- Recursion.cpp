3cfc96b57087ee888fa6415703d69a0f866a8eb8
Sreevathsa <sreevathsagowru@gmail.com>
2019-11-10 12:16:38 +0530

    Eit distance

--- Problems/Edit Distance- Recursion.cpp
+++ Problems/Edit Distance- Recursion.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
-int main(){
+int editDis(string& s1, string& s2, int m, int n){
+	
+	if(m == 0)
+		return n;	//insertions
+	if(n == 0)
+		return m;	//deletions
+		
+	if(s1[m-1] == s2[n-1])
+		return editDis(s1, s2, m-1, n-1);
+		
+	return 1 + min(editDis(s1, s2, m-1, n-1), 			//replace
+					min(editDis(s1, s2, m, n-1), 		//insert
+							editDis(s1, s2, m-1, n)));	//remove
+	
+}
 
+int main(){
+	
+	string s1, s2;
+	
+	getline(cin, s1);
+	getline(cin, s2);
+	
+	//convert s1 to s2
+	
+	cout << editDis(s1, s2, s1.length(), s2.length());
 
 	return 0;
 }
