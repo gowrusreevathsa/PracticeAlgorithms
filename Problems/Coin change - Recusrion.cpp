b82ec9cf7a3f80cfd3c7491fed4e054b78b5a60a
Sreevathsa <sreevathsagowru@gmail.com>
2019-11-10 20:06:57 +0530

    Coin Change Recursion

--- Problems/Coin change - Recusrion.cpp
+++ Problems/Coin change - Recusrion.cpp
@@ -2,8 +2,34 @@
 
 using namespace std;
 
+int count(int arr[], int len, int N){
+	
+	if(N==0)
+		return 1;	//1 solution, don't include any coin
+		
+	if(N < 0)
+		return 0;
+		
+	if(len <= 0 && N > 0){
+		return 0;
+	}
+	
+	return count(arr, len - 1, N) + count(arr, len, N - arr[len - 1]);	//excluding and including a coin
+}
+
 int main(){
 
+	int num_coins, N;
+	cin >> num_coins;
+	
+	int arr[num_coins];
+	for(int i = 0; i<num_coins; ++i){
+		cin >> arr[i];
+	}
+	
+	cin >> N;
+	
+	cout << count(arr, num_coins, N);
 
 	return 0;
 }
