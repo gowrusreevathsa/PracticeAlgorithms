1463f07620f5a136503b041cb896c7e300d1c124
Sreevathsa <sreevathsagowru@gmail.com>
2019-11-17 23:27:42 +0530

    Coin change

--- Dynamic Programming/Coin Change.cpp
+++ Dynamic Programming/Coin Change.cpp
@@ -4,17 +4,39 @@ using namespace std;
 
 int count(int arr[], int len, int N){
 	
-	if(N==0)
-		return 1;	//1 solution, don't include any coin
-		
-	if(N < 0)
-		return 0;
-		
-	if(len <= 0 && N > 0){
-		return 0;
+	int dp[len][N+1];
+	
+	for(int i = 0; i<len; ++i){
+		dp[i][0] = 1;
 	}
 	
-	return count(arr, len - 1, N) + count(arr, len, N - arr[len - 1]);	//excluding and including a coin
+	int a, b;
+	for(int i = 1; i<=N; ++i){
+		for(int j = 0; j<len; ++i){
+			
+			//including arr[j]
+			if(i - arr[j] >= 0){
+				a = dp[j][i - arr[j]];
+			}
+			else
+				a = 0;
+				
+			//excluding arr[j]
+			if(j > 0){
+				b = dp[j-1][i];
+			}
+			else{
+				b = 0;
+			}
+			
+			dp[j][i] = a + b;
+			
+			cout << dp[j][i] << " ";
+		}
+		cout << "\n";
+	}
+	
+	cout << dp[len-1][N];
 }
 
 int main(){
